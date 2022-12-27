daf33fbf1869d99a336efbb316a3a6757c2a5b19
Oxpa <saweec9@gmail.com>
2022-12-27 13:26:12 +0300

    upd 27.12

--- main/bot.py
+++ main/bot.py
@@ -3,6 +3,8 @@ from telegram.ext import CallbackQueryHandler
 import functions as f
 import config
 
+
+
 updater = Updater(config.read_token()[0])
 dp = updater.dispatcher
 
@@ -12,12 +14,13 @@ print('Bot starting')
 
 start_hand = CommandHandler('start', f.greetings)
 info_hand = CommandHandler('info', f.command_list)
+calcl_hand = CommandHandler('calc', f.calc)
 
 menu_hand = CommandHandler('menu', f.menu)
 push_button = CallbackQueryHandler(f.push)
 
-calc_hand = CommandHandler('calc',f.getMessage)
-calc_push = CallbackQueryHandler(f.calc_func)
+# calc_hand = CommandHandler('calc',f.getMessage)
+# calc_push = CallbackQueryHandler(f.calc_func)
 
 msg_hand = MessageHandler(Filters.text, f.get_msg)
 photo_hand = MessageHandler(Filters.photo, f.get_photo)
@@ -27,12 +30,13 @@ video_hand = MessageHandler(Filters.video, f.get_video)
 
 dp.add_handler(start_hand)
 dp.add_handler(info_hand)
+dp.add_handler(calcl_hand)
 
 dp.add_handler(menu_hand)
 dp.add_handler(push_button)
 
-dp.add_handler(calc_hand)
-dp.add_handler(calc_push)
+# dp.add_handler(calc_hand)
+# dp.add_handler(calc_push)
 
 dp.add_handler(msg_hand)
 dp.add_handler(photo_hand)
--- main/functions.py
+++ main/functions.py
@@ -5,9 +5,10 @@ import config
 
 
 
+
 com_list = ('Список доступных команд:\n'\
-            '/info - показать список команд\n'\
-            '/game1 - крестики нолики')
+            '/menu - показать список команд\n'\
+            '/calc [x]?[y] - просто калькулятор')
 
 def logger(update: Update, context: CallbackContext):
     config.add_log(update.message.text,\
@@ -18,7 +19,8 @@ def logger(update: Update, context: CallbackContext):
 
 
 def greetings(update: Update, context: CallbackContext):
-    update.message.reply_text('Добрый день!')
+    update.message.reply_text(f'Добрый день!')
+    update.message.reply_text(menu(update,context))
 
 def command_list(update: Update, context: CallbackContext):
     update.message.reply_text(com_list)
@@ -46,7 +48,7 @@ def test_funk():
 
 
 button_list = [
-    InlineKeyboardButton("Кнопка", callback_data='Текст'),
+    InlineKeyboardButton("Посчитаем", callback_data=f'Пример: /calc 1+2'),
     InlineKeyboardButton("Время", callback_data=f'{config.current_time()}'),
     InlineKeyboardButton("Список команд", callback_data='3')]
 
@@ -71,71 +73,106 @@ def push(update: Update, _):
 reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=2))
 
 def menu(update: Update, context: CallbackContext):
-    #context.bot.send_message(chat_id=update.message.chat_id, text="Меню", reply_markup=reply_markup)
+    # context.bot.send_message(chat_id=update.message.chat_id, text="Меню", reply_markup=reply_markup)
     update.message.reply_text('Меню:', reply_markup=reply_markup)
     push(update)
     
+def operator(n_list):
+    print(n_list)
+    print(n_list[0])
+    
+    if '-' in n_list[0]:
+        return '-'
+    elif '+' in n_list[0]:
+        return '+'
+    elif '*' in n_list[0]:
+        return '*'
+    elif '/' in n_list[0]:
+        return '/'
+
+oper = ''
+
+def calc(update: Update, context: CallbackContext):
+    global oper 
+    args = context.args
+
+    if len(args) == 1:
+        oper = operator(args)
+    if len(args) > 1:
+        args = [args[0]+args[1]+args[2]]
+        oper = operator(args)
+
+    args = str.split(args[0],f'{oper}')
+
+    if oper == '+':
+        update.message.reply_text(int(args[0])+int(args[1])) 
+    if oper == '-':
+        update.message.reply_text(int(args[0])-int(args[1])) 
+    if oper == '*':
+        update.message.reply_text(int(args[0])*int(args[1])) 
+    if oper == '/' :  
+        update.message.reply_text(int(args[0])/int(args[1])) 
 
 
-keyboard_list = [InlineKeyboardButton(' ', callback_data='no'),
-                InlineKeyboardButton('C', callback_data='C'),
-                InlineKeyboardButton('<=', callback_data='<='),
-                InlineKeyboardButton('/', callback_data='/'),
-                InlineKeyboardButton('7', callback_data='7'),
-                InlineKeyboardButton('8', callback_data='8'),
-                InlineKeyboardButton('9', callback_data='9'),
-                InlineKeyboardButton('*', callback_data='*'),
-                InlineKeyboardButton('4', callback_data='4'),
-                InlineKeyboardButton('5', callback_data='5'),
-                InlineKeyboardButton('6', callback_data='6'),
-                InlineKeyboardButton('-', callback_data='-'),
-                InlineKeyboardButton('1', callback_data='1'),
-                InlineKeyboardButton('2', callback_data='2'),
-                InlineKeyboardButton('3', callback_data='3'),
-                InlineKeyboardButton('+', callback_data='+'),
-                InlineKeyboardButton(' ', callback_data='no'),
-                InlineKeyboardButton('0', callback_data='0'),
-                InlineKeyboardButton(',', callback_data='.'),
-                InlineKeyboardButton('=', callback_data='=')]
 
-value = ''
-old_value = ''
-reply_calc = InlineKeyboardMarkup(build_menu(keyboard_list, n_cols=4))
+# keyboard_list = [InlineKeyboardButton(' ', callback_data='no'),
+#                 InlineKeyboardButton('C', callback_data='C'),
+#                 InlineKeyboardButton('<=', callback_data='<='),
+#                 InlineKeyboardButton('/', callback_data='/'),
+#                 InlineKeyboardButton('7', callback_data='7'),
+#                 InlineKeyboardButton('8', callback_data='8'),
+#                 InlineKeyboardButton('9', callback_data='9'),
+#                 InlineKeyboardButton('*', callback_data='*'),
+#                 InlineKeyboardButton('4', callback_data='4'),
+#                 InlineKeyboardButton('5', callback_data='5'),
+#                 InlineKeyboardButton('6', callback_data='6'),
+#                 InlineKeyboardButton('-', callback_data='-'),
+#                 InlineKeyboardButton('1', callback_data='1'),
+#                 InlineKeyboardButton('2', callback_data='2'),
+#                 InlineKeyboardButton('3', callback_data='3'),
+#                 InlineKeyboardButton('+', callback_data='+'),
+#                 InlineKeyboardButton(' ', callback_data='no'),
+#                 InlineKeyboardButton('0', callback_data='0'),
+#                 InlineKeyboardButton(',', callback_data='.'),
+#                 InlineKeyboardButton('=', callback_data='=')]
 
-def getMessage(update: Update, context: CallbackContext):
-    global value
-    value = context.args
-    if value == '':
-        update.message.reply_text('0', reply_markup=reply_calc)
-    else:
-        update.message.reply_text(value, reply_markup=reply_calc)
-    calc_func(update)    
+# value = ''
+# old_value = ''
+# reply_calc = InlineKeyboardMarkup(build_menu(keyboard_list, n_cols=4))
+
+# def getMessage(update: Update, context: CallbackContext):
+#     global value
+#     value = context.args
+#     if value == '':
+#         update.message.reply_text('0', reply_markup=reply_calc)
+#     else:
+#         update.message.reply_text(value, reply_markup=reply_calc)
+#     calc_func(update)    
 
 
-def calc_func(update: Update, context: CallbackContext):
-    global value, old_value
-    query = update.callback_query
-    data = query.data
-    if data == 'no':
-        pass
-    elif data == 'C':
-        value = ''
-    elif data == '<=':
-        if value != '':
-            value = value[:len(value)-1]
-    elif data == '=':
-        value = str(eval(value))
-    else:
-        value += data
-    if value != old_value:
-        if value == '':
-            context.bot.edit_message_text(
-                chat_id=query.message.chat.id, message_id=query.message.from_user.id, text='0', reply_markup=reply_calc)
-        else:
-            context.bot.edit_message_text(chat_id=query.message.chat.id,
-                                  message_id=query.message.from_user.id, text=value, reply_markup=reply_calc)
-        old_value = value
-
+# def calc_func(update: Update, context: CallbackContext):
+#     global value, old_value
+#     query = update.callback_query
+#     data = query.data
+#     if data == 'no':
+#         pass
+#     elif data == 'C':
+#         value = ''
+#     elif data == '<=':
+#         if value != '':
+#             value = value[:len(value)-1]
+#     elif data == '=':
+#         value = str(eval(value))
+#     else:
+#         value += data
+#     if value != old_value:
+#         if value == '':
+#             context.bot.edit_message_text(
+#                 chat_id=query.message.chat.id, message_id=query.message.from_user.id, text='0', reply_markup=reply_calc)
+#         else:
+#             context.bot.edit_message_text(chat_id=query.message.chat.id,
+#                                   message_id=query.message.from_user.id, text=value, reply_markup=reply_calc)
+#         old_value = value
 
 
 
